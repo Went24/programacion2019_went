537c05cd2550e7c90c1a367a4e255ac57e73c33e
went24 <a.wentchelle94@gmail.com>
2019-08-06 12:18:31 -0400

    6 Aout comparacion

--- modulo_1/intro_javasript/ejercio3_estructuras/comparacion.js
+++ modulo_1/intro_javasript/ejercio3_estructuras/comparacion.js
@@ -7,15 +7,20 @@ console.log(11%3**2); //
 
 // digitar los numeros
 
-var numero1 = prompt("Digite 1 numero ");
-var numero2 = prompt("Digite 2 numero ");
+var numero1 =parseInt(prompt("Digite numero1 "));
+var numero2 = parseInt(prompt("Digite numero2 "));
 
-var suma = parseInt(numero1) + parseInt(numero2);
-var resta = parseInt(numero1) - parseInt(numero2);
-var multiplicacion = parseInt(numero1) * parseInt(numero2);
-var division = parseInt(numero1) / parseInt(numero2);
-var residuo=parseInt(numero1) % parseInt(numero2);
-var potencia=parseInt(numero1) ** parseInt(numero2);
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+var residuo=numero1 % numero2;
+var potencia=numero1 ** numero2;
+var Igual= (10== 10);
+var mayor=(10>5);
+var menor=(10<5);
+
+/*
 
 console.log(suma);
 console.log(resta);
@@ -23,4 +28,59 @@ console.log(multiplicacion);
 console.log(division);
 console.log(residuo);
 console.log(potencia);
+console.log(Igual);
+console.log(mayor);
+console.log(menor);
+/*
+if(numero1 == numero2) {
+    console.log("son iguales");
+}
+
+if(numero1 !== numero2 ){
+    console.log("son diferentes");
+}
+
+if(numero1>numero2) {
+    console.log("mayor o que");
+}
+
+if(numero2>numero1){
+    console.log
+}
+
+if(numero1<numero2){
+    console.log("menor o que");
+}
+
+if(numero2<numero1){
+    console.log("menor que numero 1");
+}
+if(numero1>=numero2){
+    console.log("sup o igual");
+}
+
+*/
+
+
+
+//digitar los valores
+
+var numero1 = parseInt(prompt("Digite numero1 "));
+var numero2 = parseInt(prompt("Digite numero2 "));
+numero1=10;
+numero2=50;
+
+if(numero1>numero2){
+    console.log(`${numero1} es el mayor`);
+} else
+{
+    console.log(`${numero2} es el mayor`);
+}
+
+
+
+
+
+
+
 
