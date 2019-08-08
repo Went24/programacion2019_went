b31621b8542ace029ca4dfd67f65e081b491de24
went24 <a.went>
2019-08-08 12:32:56 -0400

    banca

---
+++ modulo_1/intro_javasript/ejercio3_estructuras/cajero.js
@@ -0,0 +1,60 @@
+// 0.seleccionar el idioma
+var idioma=prompt("seleccione su idioma");
+// 1.Introducir tarjeta
+var tarjeta=prompt("introduzca su tarjeta por favor");
+
+// 2.Digitar PIN
+prompt("Digite su PIN");
+ // validar que la longitud de digitos sea 4
+ //Mostrar un mensaje en caso que la longitud sea mayor 4
+ if (PIN.length > 4) {
+     alert("PIN invalido");
+    }
+    
+// 3.Mostrar opciones
+// 4.selecionar opcion
+var opcion=prompt(`Eliga opcion a realizar 
+-Retriro
+-deposito
+-balance
+-Chequear balance`);
+
+
+
+// 5.seleccionar tipo de cuenta
+var tipoCuenta= prompt("Ahorros - corriente");
+
+// 6.Mostrar opciones de monto
+var opcion=prompt(`Elija monto que quiere
+-200$
+-500$
+1000$
+2500$
+-3000$`);
+
+// 7.selecionar monto
+
+
+// 8.Mostrar opcion de recibo
+var quierecomprombante=confirm("Desea un comprobante?");
+
+//9.Elegir si o no del comprobate del recibo
+
+if(true){
+    // No imprimir recibo
+}
+
+// 10. Mostrar mensaje de retirar dinero
+alert("retira tu dinero");
+
+
+//validar que la longitud de digitos sea 12
+//Mostrar un mensaje en caso que la longitud sea mayor 12
+if(tarjeta.length > 12) {
+    alert("La tarjeta no es valida");
+
+
+}
+
+
+
