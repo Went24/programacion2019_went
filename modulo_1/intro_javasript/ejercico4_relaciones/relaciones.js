c992ad17334a8129ed826119b34ed55b14d0e618
went24 <a.wentchelle>
2019-08-09 12:24:56 -0400

    trabajo 9 auG

---
+++ modulo_1/intro_javasript/ejercico4_relaciones/relaciones.js
@@ -0,0 +1,27 @@
+
+/*
+var carro= {
+tieneBateria: false,
+ tengoLlave: true,
+
+}
+if(carro.tieneBateria && carro.tengoLalave ){
+    console.log("El carro va a encender");
+}else {
+    console.log("No enciende");
+}
+*/
+
+//poder  comer
+var mesa = {
+    hayCuchara: true,
+    hayTenedor: true,
+
+
+}
+if(mesa.hayCuchara || mesa.hayTenedor){
+    console.log("puedo comer :D");
+}
+else{
+    console.log("no puedo comer");
+}
