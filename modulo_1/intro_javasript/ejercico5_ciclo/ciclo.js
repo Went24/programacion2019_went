fc1e3e85a3a80d1a64aca8f752ae6cd7959f2ae7
went24 <a.went>
2019-08-13 12:21:25 -0400

    work Aug 13 imcomplete

---
+++ modulo_1/intro_javasript/ejercico5_ciclo/ciclo.js
@@ -0,0 +1,47 @@
+//ciclo
+for (var i=0; i<10; i++){
+    console.log(i);
+}
+
+ console.log("------------------");
+
+
+
+// for(var x=0; x<3; x++){
+     //alert(x)
+     //prompt("digite un numero positivo");}bn 
+ 
+
+ //for(var veces=1; veces<=15; veces++){
+
+     
+
+    /* if (veces == 5) {
+         continue;
+     }
+
+     document.write(`<p>soy un parrafo ${veces}</p>`);
+    }
+
+    var numero= 5;
+
+    while(numero<10){
+        console.log("soy un numero");
+        numero++;
+    }
+    */
+
+
+    
+
+    var numero=parseInt(prompt("Digite un numero"));
+    if (numero<0){
+        var numero = parseInt(prompt("Digite un numero"));
+    
+    }
+
+alert(`El numero digitado es ${numero}`);
+
+
+
+
---
+++ modulo_1/intro_javasript/ejercico5_ciclo/tarea.js
@@ -0,0 +1,12 @@
+//#1
+var cateto1=parseInt(prompt("Digite el primer cateto"));
+var cateto2 = parseInt(prompt("Digite el segundo cateto"));
+
+var potencia1=cateto1**2; //cateto1*cateto1
+var potencia2=cateto2**2;
+
+var sumaPotencia=potencia1+potencia2;
+var raizCuadrada=math.sqrt(sumaPotencia);
+alert(`la hipotenusa es ${raizCuadrada.tofixed(1)}`);
+
+var edad
